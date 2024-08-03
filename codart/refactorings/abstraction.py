143b8b8e031babd23b77bf42c787f393cfae44d8
aminhzdev <amin.hasan.zarei@gmail.com>
2024-08-03 18:47:17 +0330

    modify god class of refactoring

--- codart/refactorings/abstraction.py
+++ codart/refactorings/abstraction.py
@@ -1,10 +1,44 @@
 from abc import ABC, abstractmethod
+from codart.utility.commons.Granularity import Granularity
+
 
 class Refactoring(ABC):
+    """
+        God class of refactoring activities
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str = "", granularity: Granularity = Granularity.METHOD):
+        self.name = name
+        self.granularity = granularity
+
 
     @abstractmethod
-    def main(self, udb_path:str = "", file_path:str = "", source_class:str = "", moved_fields:str = "", moved_methods:str = "", *args, **kwargs) -> bool:
-        raise NotImplementedError("main not implement")
+    def do_refactoring(self,
+                       udb_path: str = "",
+                       file_path: str = "",
+                       *args, **kwargs) -> bool:
+        self.detect_smell()
+        self.identify_opportunities()
+        self.check_precondition()
+        self.apply_refactoring()
+        self.check_post_condition()
+
+    @abstractmethod
+    def detect_smell(self, *args, **kwargs) -> bool:
+        raise NotImplementedError(f"{type(self).__name__} not implement")
+
+    @abstractmethod
+    def identify_opportunities(self, *args, **kwargs) -> bool:
+        raise NotImplementedError(f"{type(self).__name__} not implement")
+
+    @abstractmethod
+    def check_precondition(self, *args, **kwargs) -> bool:
+        raise NotImplementedError(f"{type(self).__name__} not implement")
+
+    @abstractmethod
+    def apply_refactoring(self, *args, **kwargs) -> bool:
+        raise NotImplementedError(f"{type(self).__name__} not implement")
+
+    @abstractmethod
+    def check_post_condition(self, *args, **kwargs) -> bool:
+        raise NotImplementedError(f"{type(self).__name__} not implement")
